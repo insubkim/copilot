e868449656aeb73bf6fc9d0bb27fae0583807aa8
MoongMoongBook <j.j.kant1948@gmail.com>
2023-03-01 18:55:39 +0900

    copilot usage

---
+++ hello_world.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/*
+    코파일럿 사용법
+
+    Extentions  copilot 검색
+
+    깃 허브로 코파일럿 연동 
+
+    vscode 하단 코파일럿 로고 클릭으로 (비)활성화 선택
+
+    ctrl + enter 로 코드 만들기
+
+    accept solution 클릭 
+
+*/
+
+void    print_copilot(void)
+{
+    printf("copilot\n");
+}
+
+
+void    print_hello_world(void)
+{
+    printf("Hello World\n");
+}
+
+int main()
+{
+    print_hello_world();
+    return (0);
+}
