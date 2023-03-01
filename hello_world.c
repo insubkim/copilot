916142e497b459ac971ffc08363336ffaf2295fe
MoongMoongBook <j.j.kant1948@gmail.com>
2023-03-01 21:10:29 +0900

    tab

--- hello_world.c
+++ hello_world.c
@@ -13,7 +13,7 @@
 
     ctrl + enter 로 코드 만들기
 
-    accept solution 클릭 
+    accept solution 클릭 클락
 
 */
 
@@ -27,6 +27,43 @@ void    print_hello_world(void)
 {
     printf("Hello World\n");
 }
+
+
+int add_arr(int *arr, int size)
+{
+    int i;
+    int sum;
+
+    i = 0;
+    sum = 0;
+    while (i < size)
+    {
+        sum += arr[i];
+        i++;
+    }
+    return (sum);
+}
+
+typedef struct  s_list
+{
+    int             data;
+    struct s_list   *next;
+}               t_list;
+
+int add_list(t_list *list)
+{
+    int sum;
+
+    sum = 0;
+    while (list)
+    {
+        sum += list->data;
+        list = list->next;
+    }
+    return (sum);
+}
+
+
 
 int main()
 {
